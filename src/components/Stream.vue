9d4031f4e126417c3110596eabaef959460f00ec
blake <blake.netzeband@gmail.com>
2019-07-25 19:35:14 -0600

    Added stream components

---
+++ src/components/Stream.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <div class="graphic"></div>
+    <div class="text">
+      <p>
+        You are not the
+        <span>driftwood</span>,
+        <span>rapids</span>, or
+        <span>mud</span> of the stream.
+      </p>
+      <p>
+        You are the fish
+        <span>red</span>,
+        <span>smart</span>, and
+        <span>lean</span>.
+      </p>
+      <p>
+        You don't get dragged
+        <span>down</span>,
+        <span>down</span>, and
+        <span>under</span> by the incessant force.
+      </p>
+      <p>
+        You swim
+        <span>up</span>,
+        <span>up</span>, and
+        <span>forward</span>. You steer your own course.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script >
+export default {
+  name: "Stream",
+  props: {}
+};
+</script>
+
+<style scoped>
+section {
+  padding: 3rem;
+}
+</style>
