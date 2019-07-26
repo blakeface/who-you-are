4c0f900b73e25e0d8e209dc3e95eb87cee753b7c
blake <blake.netzeband@gmail.com>
2019-07-25 19:25:41 -0600

    move to yarn

--- src/components/TitlePage.vue
+++ src/components/TitlePage.vue
@@ -1,11 +1,9 @@
 <template>
-	<section>
-		<h1>Who You Are</h1>
-		<h3>
-			An all-age's story about people and the modern digital world
-		</h3>
-		<p>By Blake Netzeband</p>
-	</section>
+  <section>
+    <h1>Who You Are</h1>
+    <h3>An all-age's story about people and the modern digital world</h3>
+    <p>By Blake Netzeband</p>
+  </section>
 </template>
 
 <script lang="ts">
@@ -17,9 +15,9 @@ export default class TitlePage extends Vue {}
 
 <style scoped lang="scss">
 section {
-	text-align: right;
-	padding: 3rem;
-	background-color: #e0b5b5;
-	color: #000;
+  text-align: right;
+  padding: 3rem;
+  background-color: #e0b5b5;
+  color: #000;
 }
 </style>
