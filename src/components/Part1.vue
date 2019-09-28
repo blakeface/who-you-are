7ca52d06bdbe31e4157726d94c9f5da342f4a9fe
blake <blake.netzeband@gmail.com>
2019-09-27 18:45:26 -0600

    added fun shapes and randomization

--- src/components/Part1.vue
+++ src/components/Part1.vue
@@ -1,18 +1,20 @@
 <template>
-  <section>
+  <section class="part-1">
     <p>If you rolled-up all the likes, interests, thoughts, and loves of any given person</p>
-    <p>
-      <span class="shape">you'd most likely get something like this</span>
-    </p>
-    <p>
-      <span class="shape">Or this</span>
-    </p>
-    <p>
-      <span class="shape">Or this</span>
-    </p>
-    <p>
-      <span class="shape">Or this</span>
-    </p>
+    <div class="shapes-wrapper">
+      <p>
+        <span class="shape" data-shape="circle">you'd most likely get something like this</span>
+      </p>
+      <p>
+        <span class="shape" data-shape="triangle">Or this</span>
+      </p>
+      <p>
+        <span class="shape" data-shape="square">Or this</span>
+      </p>
+      <p>
+        <span class="shape" data-shape="hexagon">Or this</span>
+      </p>
+    </div>
     <canvas id="part1-canvas" resize="true" ref="part1-canvas"></canvas>
   </section>
 </template>
@@ -21,7 +23,7 @@
 import paper from "paper";
 
 export default {
-  name: "TitlePage",
+  name: "Part1",
   data() {
     return {
       paper: null,
@@ -31,10 +33,75 @@ export default {
   },
 
   methods: {
-    getRandomColor: function() {
+    getRandomHex: function() {
       return ("00000" + ((Math.random() * (1 << 24)) | 0).toString(16)).slice(
         -6
       );
+    },
+
+    randomizeParagraphAlignment: function() {
+      const paragraphs = document.querySelectorAll("p");
+      [...paragraphs].forEach(el => {
+        const random = Math.random() * 3;
+        const align = random < 1 ? "left" : random > 2 ? "right" : "center";
+        return el.setAttribute("style", `text-align:${align};`);
+      });
+    },
+
+    drawShapes: function() {
+      const elements = document.querySelectorAll(".shape");
+      [...elements].forEach((el, i) => {
+        // draw shape
+        const { right: x, top: y } = el.getBoundingClientRect();
+        const type = el.dataset.shape;
+
+        const shape = this.getShape(type, x, y);
+        this.shapes[i] = shape;
+      });
+    },
+
+    getShape: function(type, x, y) {
+      const radius = 50;
+      // compensate for font and screen position
+      y = y + 15 - this.yOffset;
+      x = x + radius * 1.5;
+
+      const center = new this.paper.Point(x, y);
+      const fillColor = `#${this.getRandomHex()}`;
+      let sides = 0;
+
+      if (type === "circle") {
+        return new this.paper.Path.Circle({
+          center,
+          radius,
+          fillColor
+        });
+      }
+
+      switch (type) {
+        case "hexagon":
+          sides = 6;
+          break;
+        case "triangle":
+          sides = 3;
+          break;
+        case "square":
+          sides = 4;
+          break;
+      }
+
+      return new this.paper.Path.RegularPolygon({
+        center,
+        sides,
+        radius,
+        fillColor
+      });
+    },
+
+    handleSectionClick: function() {
+      this.randomizeParagraphAlignment();
+      this.shapes.forEach(shape => shape.remove());
+      this.drawShapes();
     }
   },
 
@@ -45,18 +112,15 @@ export default {
     const { y: yOffset } = this.$refs["part1-canvas"].getBoundingClientRect();
     this.yOffset = yOffset;
 
-    // draw first shapes
-    const elements = document.querySelectorAll(".shape");
-    [...elements].forEach((el, i) => {
-      const { right: x, top: y } = el.getBoundingClientRect();
+    // randomize paragraph alignment
+    this.randomizeParagraphAlignment();
 
-      const shape = new this.paper.Shape.Circle(
-        new this.paper.Point(x, y - this.yOffset),
-        15
-      );
-      shape.strokeColor = this.getRandomColor();
-      this.shapes[i] = shape;
-    });
+    // add click events
+    const section = document.querySelector(".part-1");
+    section.addEventListener("click", this.handleSectionClick);
+
+    // draw first shapes
+    this.drawShapes();
   }
 };
 </script>
@@ -64,12 +128,29 @@ export default {
 <style scoped>
 section {
   position: relative;
+  min-height: 100vh;
+  background-color: #b6e0c2;
+  padding: 1rem;
 }
+section:hover {
+  cursor: pointer;
+}
+
 canvas {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
+}
+
+p {
+  font-size: 20px;
+  padding: 50px 0;
+  margin: 0;
+}
+
+.shapes-wrapper {
+  max-width: calc(100% - 150px);
 }
 </style>
