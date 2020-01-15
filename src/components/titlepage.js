3ff078cfe946d09bfed5f3e6f7df26c5b21ef090
blake <blake.netzeband@gmail.com>
2020-01-15 07:53:12 -0700

    removed preact-router. removed unused components. added TitlePage componenet

---
+++ src/components/titlepage.js
@@ -0,0 +1,9 @@
+export default function Titlepage() {
+  return (
+    <section>
+      <h1>Who You Are</h1>
+      <h3>The shapes and colors of people in the modern digital world</h3>
+      <p>By Blake Netzeband</p>
+    </section>
+  );
+}
