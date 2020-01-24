fa0b81ae79989c77ba0a387309f941cd2a591367
blake <blake.netzeband@gmail.com>
2020-01-23 17:11:00 -0700

    added basic animation

--- src/components/titlepage.js
+++ src/components/titlepage.js
@@ -1,16 +1,27 @@
+import { useEffect } from "preact/hooks";
 import anime from "animejs";
 
 import TitleSvg from "./titleSvg";
 
+const targets = "svg";
+
 export default function TitlePage() {
+  useEffect(() => {
+    anime({
+      targets,
+      translateX: 500,
+      loop: true,
+      delay: anime.stagger(500, { easing: "easeOutQuad" }),
+      direction: "alternate"
+    });
+  }, []);
+
   return (
     <section id="title-page">
       <div class="svg-wrapper">
-        <TitleSvg />
-        <TitleSvg />
-        <TitleSvg />
-        <TitleSvg />
-        <TitleSvg />
+        {Array.from({ length: 5 }).map(_ => (
+          <TitleSvg class={targets} />
+        ))}
       </div>
 
       <h3>Or who you are in the digital world</h3>
