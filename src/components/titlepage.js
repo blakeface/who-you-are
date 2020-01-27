b50926fe5c7ffc69ffc4d9fa3d09e7e67b3b48f1
blake <blake.netzeband@gmail.com>
2020-01-26 18:23:33 -0700

    added style to title page

--- src/components/titlepage.js
+++ src/components/titlepage.js
@@ -1,31 +1,43 @@
-import { useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 import anime from "animejs";
 
 import TitleSvg from "./titleSvg";
+import { Fragment } from "preact";
 
 const targets = "svg";
+const svgHeight = 150;
 
 export default function TitlePage() {
+  const [lineCount, setLineCount] = useState(4);
   useEffect(() => {
-    anime({
-      targets,
-      translateX: 500,
-      loop: true,
-      delay: anime.stagger(500, { easing: "easeOutQuad" }),
-      direction: "alternate"
-    });
+    setLineCount(Math.max(Math.ceil(window.outerHeight / svgHeight), 4));
   }, []);
+  useEffect(() => {
+    // anime({
+    //   targets: targets + " path",
+    //   strokeDashoffset: [anime.setDashoffset, 0],
+    //   easing: "easeInOutSine",
+    //   duration: 1000,
+    //   delay: (_, i) => i * 50,
+    //   loop: false
+    // });
+  }, [lineCount]);
 
   return (
     <section id="title-page">
       <div class="svg-wrapper">
-        {Array.from({ length: 5 }).map(_ => (
-          <TitleSvg class={targets} />
+        {Array.from({ length: lineCount }).map((_, i) => (
+          <div class="inline-svg">
+            <TitleSvg class={targets} />
+            <TitleSvg class={targets} />
+          </div>
         ))}
       </div>
 
-      <h3>Or who you are in the digital world</h3>
-      <p>By Blake Netzeband</p>
+      <div class="text-wrapper">
+        <h3>Or who you are in the digital world</h3>
+        <p>By Blake Netzeband</p>
+      </div>
     </section>
   );
 }
